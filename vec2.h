d2c987a55c513d93daadc13652e2ac2e73567d5a
Axel Nunez <axnunezc@gmail.com>
2024-09-09 21:43:15 -0400

    002.001 Create BaseVec2 class

---
+++ vec2.h
@@ -0,0 +1,14 @@
+// vec2.h
+#ifndef VEC2_H
+#define VEC2_H
+
+#include <cmath>
+#include <iostream>
+
+template <typename T>
+class BaseVec2 {
+    protected:
+        T components [2];
+};
+
+#endif
