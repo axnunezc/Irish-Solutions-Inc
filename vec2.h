5197e7daad4e9ebb8eca00e76f19d92122016a3d
Axel Nunez <axnunezc@gmail.com>
2024-09-09 21:56:17 -0400

    002.006 Create vec2 and ivec2 classes using the BaseVec2 template

--- vec2.h
+++ vec2.h
@@ -17,4 +17,17 @@ class BaseVec2 {
         BaseVec2& operator=(const BaseVec2& other);
 };
 
+class vec2 : public BaseVec2<float> {
+public:
+    vec2();
+    vec2(float x, float y);
+};
+
+class ivec2 : public BaseVec2<int> {
+public:
+    ivec2();
+    ivec2(int x, int y);
+    int magnitude() const; // int magnitude
+};
+
 #endif
