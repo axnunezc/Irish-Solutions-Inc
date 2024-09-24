3402cc32b01ddee122634a515baaf151715716ea
Axel Nunez <axnunezc@gmail.com>
2024-09-24 16:34:24 -0400

    003.005 Write constructor for Screen class

--- screen.hpp
+++ screen.hpp
@@ -9,6 +9,16 @@ class Screen {
         SDL_Surface* surface;
 
     public:
+        // Class constructor
+        Screen(unsigned int _width, unsigned int _height) : width(_width), height(_height) {
+            surface = SDL_CreateRGBSurface(0, width, height, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
+            if (!surface) {
+                std::cerr << "Failed to create SDL surface: " << SDL_GetError() << std::endl;
+            }
+        }
+
+        // Class destructor
+
         // Color a single pixel
         void setPixel(const ivec2& pos, const ivec3& color) {
             if (pos.x >= 0 && pos.x < width && pos.y >= 0 && pos.y < height) {
