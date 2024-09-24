3fb55465c1268e34d1c90faee29a312324fc68a0
Axel Nunez <axnunezc@gmail.com>
2024-09-24 16:23:48 -0400

    003.003 Write method to color a single pixel

--- screen.hpp
+++ screen.hpp
@@ -5,8 +5,16 @@
 
 class Screen {
     private:
-    unsigned int width, height;
-    SDL_Surface* surface;
+        unsigned int width, height;
+        SDL_Surface* surface;
 
     public:
+        // Color a single pixel
+        void setPixel(const ivec2& pos, const ivec3& color) {
+            if (pos.x >= 0 && pos.x < width && pos.y >= 0 && pos.y < height) {
+                uint32_t* pixels = (uint32_t*)surface->pixels;
+                uint32_t pixelColor = SDL_MapRGB(surface->format, color.x, color.y, color.z);
+                pixels[pos.y * width + pos.x] = pixelColor;
+            }
+        }
 };
