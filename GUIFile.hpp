aa063785f511f1a0b297be4a2877726b84150b9c
Axel Nunez <axnunezc@gmail.com>
2024-10-06 15:45:06 -0400

    004.001 Create GUIFile class

---
+++ GUIFile.hpp
@@ -0,0 +1,17 @@
+#ifndef GUIFILE_HPP
+#define GUIFILE_HPP
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "vec2.hpp"
+#include "vec3.hpp"
+
+class GUIFile {
+public:
+    void clear();
+
+};
+
+#endif // GUIFILE_HPP
