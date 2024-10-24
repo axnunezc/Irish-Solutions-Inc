9437eb3ff6e781a6ed79790d4897be79289dc29b
Axel Nunez <axnunezc@gmail.com>
2024-10-23 22:59:40 -0400

    005.003 Create draw() function override for Box

--- element.hpp
+++ element.hpp
@@ -23,6 +23,19 @@ private:
 public:
     Box(vec2 _min, vec2 _max, vec3 _color)
         : min(_min), max(_max), color(_color) {}
+
+    void draw(Screen& screen) override {
+        int x1 = static_cast<int>(std::round(std::min(min.x, max.x)));
+        int y1 = static_cast<int>(std::round(std::min(min.y, max.y)));
+        int x2 = static_cast<int>(std::round(std::max(min.x, max.x)));
+        int y2 = static_cast<int>(std::round(std::max(min.y, max.y)));
+
+        for (int y = y1; y <= y2; ++y) {
+            for (int x = x1; x <= x2; ++x) {
+                screen.setPixel(vec2(static_cast<float>(x), static_cast<float>(y)), color);
+            }
+        }
+    }
 };
 
 #endif // ELEMENT_HPP
