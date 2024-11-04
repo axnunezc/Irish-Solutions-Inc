aa345959de07942d6b1bd6bd2ff4ab98ae88dcb7
Axel Nunez <axnunezc@gmail.com>
2024-11-03 23:13:52 -0500

    006.002 Create addElement, addLayout, and setActive functions

--- layout.hpp
+++ layout.hpp
@@ -12,6 +12,18 @@ public:
     Layout(const ivec2& start = {0, 0}, const ivec2& end = {0, 0}, bool isActive = true)
         : startPosition(start), endPosition(end), active(isActive) {}
 
+    void addElement(std::shared_ptr<Element> element) {
+        elements.push_back(element);
+    }
+
+    void addNestedLayout(std::shared_ptr<Layout> layout) {
+        nestedLayouts.push_back(layout);
+    }
+
+    void setActive(bool state) {
+        active = state;
+    }
+
 private:
     ivec2 startPosition;
     ivec2 endPosition;
