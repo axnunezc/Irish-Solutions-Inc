7176ae36218436a715271d3501e3bd68e3a08ef0
Axel Nunez <axnunezc@gmail.com>
2024-11-12 16:31:51 -0500

    007.002 Create derived Event classes

--- event.hpp
+++ event.hpp
@@ -11,4 +11,27 @@ public:
     virtual EventType getType() const = 0;
 };
 
+class ClickEvent : public Event {
+    int x, y;
+public:
+    ClickEvent(int xCoord, int yCoord) : x(xCoord), y(yCoord) {}
+    EventType getType() const override { return EventType::CLICK; }
+    int getX() const { return x; }
+    int getY() const { return y; }
+};
+
+class ShowEvent : public Event {
+    std::string layoutName;
+public:
+    ShowEvent(const std::string &name) : layoutName(name) {}
+    EventType getType() const override { return EventType::SHOW; }
+    const std::string& getLayoutName() const { return layoutName; }
+};
+
+class SoundEvent : public Event {
+public:
+    SoundEvent() {}
+    EventType getType() const override { return EventType::SOUND; }
+};
+
 #endif // EVENT_HPP
